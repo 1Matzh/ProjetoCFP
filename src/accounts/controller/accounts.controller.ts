be02aa8cd0ff6fdc216c6557be6265e4a12c6329
1Matzh <matheusbenasse@gmail.com>
2024-05-17 19:10:10 -0300

    Implementado Autenticação para Todos os Métodos de Conta Financeira

--- src/accounts/controller/accounts.controller.ts
+++ src/accounts/controller/accounts.controller.ts
@@ -22,22 +22,30 @@ import { Request } from 'express';
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 export class AccountsController {
-  constructor(private readonly accountsService: AccountsService) { }
+  constructor(private readonly accountsService: AccountsService) {}
+
+  @Get()
+  async findAllByUser(@Req() req: Request): Promise<any[]> {
+    const userId = req.user.userId;
+    return this.accountsService.findAllByUser(userId);
+  }
 
   @Post()
-  async create(@Body() createAccountDto: CreateAccountDto, @Req() req: Request) {
+  async create(@Body() createAccountDto: CreateAccountDto, @Req() req: Request): Promise<any> {
     const userId = req.user.userId;
     return this.accountsService.create(userId, createAccountDto);
   }
 
   @Put(':id')
-  async update(@Param('id') id: number, @Body() updateAccountDto: UpdateAccountDto) {
-    return this.accountsService.update(id, updateAccountDto);
+  async update(@Param('id') id: number, @Body() updateAccountDto: UpdateAccountDto, @Req() req: Request): Promise<any> {
+    const userId = req.user.userId;
+    return this.accountsService.update(userId, id, updateAccountDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: number): Promise<void> {
-    return this.accountsService.remove(id);
+  async remove(@Param('id') id: number, @Req() req: Request): Promise<void> {
+    const userId = req.user.userId;
+    return this.accountsService.remove(userId, id);
   }
 }
 
