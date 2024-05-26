4f2d12110bda624ae9a522f9cc5f78cc793004bf
Gustavo-FerC <164891562+Gustavo-FerC@users.noreply.github.com>
2024-05-26 13:17:27 -0300

    Update accounts.module.ts

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -7,6 +7,7 @@ import { AccountsService } from './service/accounts.service';
 import { TransactionsService } from './transactions/service/transactions.service';
 import { TransactionsController } from './transactions/controller/transactions.controller';
 import { UserModule } from 'src/user/user.module';
+import { ReportsService } from '../service/reports.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Accounts, Transactions]), forwardRef(() => UserModule)],
